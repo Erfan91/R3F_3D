407d871c39b200e443c676f06ffd900d42802cf9
Erfan91 <sadidhadix@gmail.com>
2025-12-11 15:27:00 +0100

    drei a r3f helper library added, directional lights modified, r3f fundamentals

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useRef, useState } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import './App.css'
-import { Mesh } from 'three/src/Three.Core.js';
+import { MeshWobbleMaterial, OrbitControls, useHelper } from '@react-three/drei'
+import { DirectionalLightHelper } from 'three';
 
 const Cube = ({ position, size, color }) => {
   const ref = useRef();
@@ -32,21 +33,21 @@ const Sphere = ({ position, size, color }) => {
 
 
   useFrame((state, delta) => {
-      const speed = isHovered ? 1 : 0.2;
-      ref.current.rotation.y += delta * speed;
-      })
+    const speed = isHovered ? 1 : 0.2;
+    ref.current.rotation.y += delta * speed;
+  })
 
   return (
-    <mesh 
-    position={position}
-    ref={ref}
-    onPointerEnter={(e) => {e.stopPropagation(), setIsHovered(true) }}
-    onPointerLeave={() => setIsHovered(false)}
-    onClick={() => setIsClicked(!isClicked)}
-    scale={isClicked ? 1.5 : 2}
+    <mesh
+      position={position}
+      ref={ref}
+      onPointerEnter={(e) => { e.stopPropagation(), setIsHovered(true) }}
+      onPointerLeave={() => setIsHovered(false)}
+      onClick={() => setIsClicked(!isClicked)}
+      scale={isClicked ? 1.5 : 2}
     >
       <sphereGeometry args={size} />
-      <meshStandardMaterial color={isHovered ? "skyblue" : "orange"} wireframe/>
+      <meshStandardMaterial color={isHovered ? "skyblue" : "orange"} wireframe />
     </mesh>
   )
 }
@@ -56,7 +57,7 @@ const Torus = ({ position, size, color }) => {
   useFrame((state, delta) => {
     ref.current.rotation.x += delta;
     ref.current.rotation.y += delta;
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) *2;
+    ref.current.position.z = Math.sin(state.clock.elapsedTime) * 2;
 
   })
   return (
@@ -70,18 +71,47 @@ const Torus = ({ position, size, color }) => {
 const TorusKnot = ({ position, size, color }) => {
   const ref = useRef();
 
-  useFrame((state, delta) => {
-    ref.current.rotation.x += delta;
-    ref.current.rotation.y += delta;
-    ref.current.position.z = Math.sin(state.clock.elapsedTime) *2;
 
-  })
+  // useFrame((state, delta) => {
+  //   ref.current.rotation.x += delta;
+  //   ref.current.rotation.y += delta;
+  //   ref.current.position.z = Math.sin(state.clock.elapsedTime) *2;
+
+  // })
 
   return (
     <mesh position={position} ref={ref}>
       <torusKnotGeometry args={size} />
-      <meshStandardMaterial color={color} />
+      {/* <meshStandardMaterial color={color} /> */}
+      <MeshWobbleMaterial factor={5} speed={2} />
     </mesh>
+  )
+}
+
+const Scene = () => {
+
+  const directionalLightRef = useRef();
+  useHelper(directionalLightRef, DirectionalLightHelper, .5, "blue")
+
+  return (
+    <>
+      {/*Add some lights to make mesh visible */}
+      <directionalLight position={[0, 0, 2]} ref={directionalLightRef}/>
+      <ambientLight intensity={0.5} />
+      {/*We can create a group, by adding group tag which is like dev tag we change the position and other properties direcly from group tag*/}
+
+      {/* <group position={[0,-1, 0]}>
+        <Cube position={[1, 0, 0]} size={[1,1,1]} color={"red"}/>
+        <Cube position={[-1, 0, 0]} size={[1,1,1]} color={"green"}/>
+        <Cube position={[1, 2, 0]} size={[1,1,1]} color={"blue"}/>
+        <Cube position={[-1, 2, 0]} size={[1,1,1]} color={"yellow"}/>
+    </group> */}
+      {/* <Cube position={[0, 0, 0]} size={[1,1,1]} color={"orange"}/> */}
+      {/* <Sphere position={[0,0,0]} size={[1,32,32]} color={'orange'}/> */}
+      {/* <Torus position={[2.2, 0, 0]} size={[.7, 0.3, 16, 100]} color={"pink"} /> */}
+      <TorusKnot position={[0, 0, 0]} size={[1, .1, 1000, 50]} color={"violet"} />
+      <OrbitControls />
+    </>
   )
 }
 
@@ -90,21 +120,7 @@ function App() {
   return (
     <>
       <Canvas>
-        {/*Add some lights to make mesh visible */}
-        <directionalLight position={[0, 0, 2]} />
-        <ambientLight intensity={0.5} />
-        {/*We can create a group, by adding group tag which is like dev tag we change the position and other properties direcly from group tag*/}
-
-        {/* <group position={[0,-1, 0]}>
-        <Cube position={[1, 0, 0]} size={[1,1,1]} color={"red"}/>
-        <Cube position={[-1, 0, 0]} size={[1,1,1]} color={"green"}/>
-        <Cube position={[1, 2, 0]} size={[1,1,1]} color={"blue"}/>
-        <Cube position={[-1, 2, 0]} size={[1,1,1]} color={"yellow"}/>
-    </group> */}
-        {/* <Cube position={[0, 0, 0]} size={[1,1,1]} color={"orange"}/> */}
-        <Sphere position={[0,0,0]} size={[1,32,32]} color={'orange'}/>
-        {/* <Torus position={[2.2, 0, 0]} size={[.7, 0.3, 16, 100]} color={"pink"} /> */}
-        {/* <TorusKnot position={[-2,0,0]} size={[.5, .1, 1000, 50]} color={"violet"}/> */}
+        <Scene/>
       </Canvas>
     </>
   )
